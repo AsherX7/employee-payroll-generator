ddf8079e987b54f9b7091daaea1020e8d232b531
AsherX7 <angelakaby6@gmail.com>
2026-06-01 13:49:17 +0530

    Initial commit from Eclipse

--- src/model/Employee.java
+++ src/model/Employee.java
@@ -1,1 +1,84 @@
+package model;
+/*
+ * Base Employee class
+ * Stores common employee details.
+ */
 
+public class Employee {
+
+    // Attributes
+    private int empId;
+    private String name;
+    private String employeeType;
+    private double basicSalary;
+    private int daysWorked;
+   
+    // Default Constructor
+    public Employee() {
+
+    }
+
+    // Parameterized Constructor
+    public Employee(int empId, String name, String employeeType,
+                    double basicSalary,int daysWorked) {
+
+        this.empId = empId;
+        this.name = name;
+        this.employeeType = employeeType;
+        this.basicSalary = basicSalary;
+        this.daysWorked=daysWorked;
+       
+    }
+
+    // Getters and Setters
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmployeeType() {
+        return employeeType;
+    }
+
+    public void setEmployeeType(String employeeType) {
+        this.employeeType = employeeType;
+    }
+
+    public double getBasicSalary() {
+        return basicSalary;
+    }
+
+    public void setBasicSalary(double basicSalary) {
+        this.basicSalary = basicSalary;
+    }
+
+    public int getDaysWorked() {
+        return daysWorked;
+    }
+
+    public void setDaysWorked(int daysWorked) {
+        this.daysWorked = daysWorked;
+    }
+
+    // Display Employee Details
+    public void displayEmployeeDetails() {
+
+        System.out.println("Employee ID : " + empId);
+        System.out.println("Name        : " + name);
+        System.out.println("Type        : " + employeeType);
+        System.out.println("Salary      : " + basicSalary);
+        System.out.println("Days Worked : " + daysWorked);
+    }
+}
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,19 +1,25 @@
 
-package in.ac.tkmit.payroll;
+package ui;
 import java.util.Scanner;
 
-public class EmployeeInput {
 
+/**
+ * 
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// EMPLOYEE PAYROLL GENERATOR UI AREA
+		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
 		System.out.println("=========================================================");
 		System.out.println("                   ADD EMPLOYEE");
 		System.out.println("=========================================================");
 		// Input Fields
 		System.out.print("Enter the Employee id: ");
-		System.out.println(" ");
-		int id= sc.nextInt();
+		String id= sc.nextLine();
 		sc.nextLine();
 		
 		System.out.println("Enter the Employee Name: ");
@@ -21,29 +27,49 @@ public class EmployeeInput {
 		
 		System.out.println("Enter the Department: ");
 		String department=sc.nextLine();
+		System.out.println("Enter the  Employee Experience(years): ");
+		int experience=sc.nextInt();
 		
 		System.out.println("Select Employee Type");
 		System.out.println("1.Full Time");
 		System.out.println("2.Part Time");
 		System.out.println("Enter Choice:");
 		int choice=sc.nextInt();
-		sc.nextLine();
-		String employeeType;
+		double basicSalary=0;
+		double allowance=0;
+		double deduction=0;
+		int hoursWorked=0;
+		double hourlyRate=0;
+		double salary=0;
+	
 		if(choice==1) {
-			employeeType="Full Time";
+		System.out.println("Basic Salary");
+		basicSalary=sc.nextDouble();
+			System.out.println("Enter Allowance:");
+			 allowance=sc.nextDouble();
+			System.out.println("Enter Deduction:");
+			 deduction=sc.nextDouble();
+			System.out.println("Enter the Salary:");
+			 salary=sc.nextDouble();
+			
+			
+			
 		}
 		else if(choice==2) {
-			employeeType="Part Time";
+			
+			System.out.println("Enter hours Worked:");
+			 hoursWorked=sc.nextInt();
+			System.out.println("Enter Hourly Rate:");
+			hourlyRate=sc.nextDouble();
+			System.out.println("Enter the Salary:");
+			 salary=sc.nextDouble();
+			
+			
 		}
 		else {
-			employeeType="Invalid";
+			System.out.println("Invalid Choice");
+			return;
 		}
-		System.out.println("Enter the  Employee Experience(years): ");
-		int experience=sc.nextInt();
-		sc.nextLine();
-		
-		System.out.println("Enter Salary: ");
-		double salary=sc.nextDouble();
 		
 		
 		System.out.println("\n Employee Added Successfully");
@@ -51,13 +77,20 @@ public class EmployeeInput {
 		System.out.println("Employee id:"+id);
 		System.out.println("Employee Name:"+name);
 		System.out.println("Deapartment:"+department);
-		System.out.println("Employee Type:"+employeeType);
 		System.out.println("Experience:"+experience + "years");
-		System.out.println("Salary:"+salary);
-	
 		
-		
-
+		if(choice==1) {
+			System.out.println("Employee Type:Full Time");
+			System.out.println("Basic Salary:"+ basicSalary);
+			System.out.println("Allowance:"+ allowance);
+			System.out.println("Deduction:"+ deduction);
+		}
+		else {
+			System.out.println("Employee Type: Part Time");
+			System.out.println("Hours Worked:"+hoursWorked);
+			System.out.println("Hourly Rate:"+hourlyRate);
+		}
+		System.out.println("Salary:"+ salary);
 	}
-
 }
+		
