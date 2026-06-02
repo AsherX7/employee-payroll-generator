461e0a78ea74af9ceb7d040cf005d846d6807574
AsherX7 <angelakaby6@gmail.com>
2026-06-02 18:15:36 +0530

    PUSHING allowance and payroll

--- src/dao/AllowanceDAO.java
+++ src/dao/AllowanceDAO.java
@@ -1,1 +1,24 @@
+package dao;
 
+import java.sql.PreparedStatement;
+public class AllowanceDAO{
+public void addAllowance(
+        int empId,
+        String allowanceName,
+        double amount) {
+    Connection conn = DBConnection.getConnection();
+    String sql =
+        "INSERT INTO employee_allowances " +
+        "(emp_id, allowance_name, amount) " +
+        "VALUES (?, ?, ?)";
+
+    PreparedStatement ps =
+        conn.prepareStatement(sql);
+
+    ps.setInt(1, empId);
+    ps.setString(2, allowanceName);
+    ps.setDouble(3, amount);
+
+    ps.executeUpdate();
+}
+}
