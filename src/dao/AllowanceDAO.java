31575bcee389cc803e4cab30cd47d99036efc6f8
AsherX7 <angelakaby6@gmail.com>
2026-06-02 18:31:57 +0530

    edited allowance dao

--- src/dao/AllowanceDAO.java
+++ src/dao/AllowanceDAO.java
@@ -2,11 +2,12 @@ package dao;
 
 import java.sql.PreparedStatement;
 public class AllowanceDAO{
+	Connection conn = DBConnection.getConnection();
 public void addAllowance(
         int empId,
         String allowanceName,
         double amount) {
-    Connection conn = DBConnection.getConnection();
+    
     String sql =
         "INSERT INTO employee_allowances " +
         "(emp_id, allowance_name, amount) " +
@@ -18,6 +19,25 @@ public void addAllowance(
     ps.setInt(1, empId);
     ps.setString(2, allowanceName);
     ps.setDouble(3, amount);
+
+    ps.executeUpdate();
+}
+public void updateAllowance(
+        int empId,
+        String allowanceName,
+        double amount) {
+    
+    String sql =
+        "UPDATE employee_allowances " +
+        "SET allowance_name=?,amount=? WHERE emp_id=?";
+
+    PreparedStatement ps =
+        conn.prepareStatement(sql);
+
+    ps.setString(1, allowanceName); 
+    ps.setDouble(2, amount);         
+    ps.setInt(3, empId);            
+
 
     ps.executeUpdate();
 }
