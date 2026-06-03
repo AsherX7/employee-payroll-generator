97ea42204bb74e55c124fe950f667293bdef2857
AsherX7 <angelakaby6@gmail.com>
2026-06-03 10:13:31 +0530

    PAYROLLSERVICE AND ALLOWANCEDAO DONE

--- src/dao/AllowanceDAO.java
+++ src/dao/AllowanceDAO.java
@@ -1,8 +1,10 @@
 package dao;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 public class AllowanceDAO{
 	Connection conn = DBConnection.getConnection();
+	// FOR ADDING ALLOWANCES TO THE TABLE
 public void addAllowance(
         int empId,
         String allowanceName,
@@ -13,8 +15,7 @@ public void addAllowance(
         "(emp_id, allowance_name, amount) " +
         "VALUES (?, ?, ?)";
 
-    PreparedStatement ps =
-        conn.prepareStatement(sql);
+    PreparedStatement ps =conn.prepareStatement(sql);
 
     ps.setInt(1, empId);
     ps.setString(2, allowanceName);
@@ -22,6 +23,7 @@ public void addAllowance(
 
     ps.executeUpdate();
 }
+// FOR UPDATING TABLE
 public void updateAllowance(
         int empId,
         String allowanceName,
@@ -41,4 +43,41 @@ public void updateAllowance(
 
     ps.executeUpdate();
 }
+// TO GET TOTAL ALLOWANCE FOR AN EMPLOYEE
+public static double gettotalAllowance(int empId)
+         {
+    String sql =
+        "SELECT SUM(allowance_amount) FROM  employee_allowances WHERE empID= ?" ;
+
+    PreparedStatement ps =
+        conn.prepareStatement(sql);
+
+    ps.setInt(1, empId);
+    ResultSet rs =ps.executeQuery();
+    if(rs.next()) {
+    	return rs.getDouble("SUM(allowance_amount)");
+    }
+   
+         }
+
+// total fulltimers & parttimers deduction separately
+
+public static  double gettaxPercentage(String tax_name,String emp_type)
+{
+String sql =
+"SELECT tax_per FROM  payroll WHERE tax_name= ? and emp_type= ?" ;
+
+PreparedStatement ps =
+conn.prepareStatement(sql);
+
+ps.setString(1, tax_name);
+ps.setString(2, emp_type );
+
+ResultSet rs =ps.executeQuery();
+if(rs.next()) {
+	return rs.getDouble("tax_per");
+}
+
+}
+
 }
--- src/service/PayrollService.java
+++ src/service/PayrollService.java
@@ -1,5 +1,57 @@
 package service;
 
+import dao.AllowanceDAO;
+
+import model.Employee;
+
 public class PayrollService{
+	// SALARY CALCULATION
+	Employee emp=new Employee();
+	public double calculateNetSalary(Employee emp) {
 	
-}
+		// salary from Employee in basicSalary
+		
+		double basicSalary= emp.getSalary();
+		
+		// to get total allowance
+		
+		double allowanceTotal= AllowanceDAO.gettotalAllowance(emp.getEmpId());
+		
+		// adding allowance to basic salary
+		
+		double grossSalary= basicSalary + allowanceTotal;
+		
+		//deductions
+		
+	    double deductionTotal=0;
+	    double altaX=0;
+	    if (emp.getEmpType().toUpperCase().equals("FULLTIME")) {
+	    	double IT=basicSalary*(AllowanceDAO.gettaxPercentage("INCOME TAX","FULLTIME"))/100;
+	    	double PT=basicSalary*(AllowanceDAO.gettaxPercentage("PROFESSIONAL TAX","FULLTIME"))/100;
+	    	double PF=basicSalary*(AllowanceDAO.gettaxPercentage("PROVIDENT FUND","FULLTIME"))/100;
+	    	double ESI=basicSalary*(AllowanceDAO.gettaxPercentage("ESI","FULLTIME"))/100;
+	    	double HI=basicSalary*(AllowanceDAO.gettaxPercentage("HEALTH INSURANCE","FULLTIME"))/100;
+	    	double PC=basicSalary*(AllowanceDAO.gettaxPercentage("PENSION CONTRIBUTION","FULLTIME"))/100;
+	    	//ADDING
+	    	deductionTotal=IT+PT+PF+ESI+HI+PC;
+	    	// ALLOWANCE
+	    	double HRA=basicSalary*(AllowanceDAO.gettaxPercentage("HRA","FULLTIME"))/100;
+	    	double IA=basicSalary*(AllowanceDAO.gettaxPercentage("INTERNET ALLOWANCE","FULLTIME"))/100;
+             altaX=HRA+IA;
+	    }
+	    else
+	    {
+	    	//deduction
+	    	double IT=basicSalary*(AllowanceDAO.gettaxPercentage("INCOME TAX","PARTTIME"))/100;
+	    	double PT=basicSalary*(AllowanceDAO.gettaxPercentage("PROFESSIONAL TAX","PARTTIME"))/100;
+	    	deductionTotal=IT+PT;
+	    	//allowance
+	    	double IA=basicSalary*(AllowanceDAO.gettaxPercentage("INTERNET ALLOWANCE","PARTTIME"))/100;
+	    	altaX=IA;
+	    }
+	    double netSalary=grossSalary-deductionTotal+altaX;
+		return netSalary;
+	    }
+	    
+		}
+
