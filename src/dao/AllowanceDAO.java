764be0581d4cbce5a897135bf7f61ecc7fed821d
AsherX7 <angelakaby6@gmail.com>
2026-06-04 15:06:11 +0530

    NEW ALLOWANCEDAO AND PAYROLLSERVICE

--- src/dao/AllowanceDAO.java
+++ src/dao/AllowanceDAO.java
@@ -1,92 +1,106 @@
 package dao;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-public class AllowanceDAO{
-	Connection conn = DBConnection.getConnection();
-	// FOR ADDING ALLOWANCES TO THE TABLE
-public void addAllowance(
-        int empId,
-        String allowanceName,
-        double amount) {
-    
-    String sql =
-        "INSERT INTO employee_allowances " +
-        "(emp_id, allowance_name, amount) " +
-        "VALUES (?, ?, ?)";
 
-    PreparedStatement ps;
-	try {
-		ps = conn.prepareStatement(sql);
-    ps.setInt(1, empId);
-    ps.setString(2, allowanceName);
-    ps.setDouble(3, amount);
+public class AllowanceDAO {
 
-    ps.executeUpdate();
-	} catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-}
-// FOR UPDATING TABLE
-public void updateAllowance(
-        int empId,
-        String allowanceName,
-        double amount) {
-    
-    String sql =
-        "UPDATE employee_allowances " +
-        "SET allowance_name=?,amount=? WHERE emp_id=?";
+    Connection conn = DBConnection.getConnection();
 
-    try {
-    PreparedStatement ps= conn.prepareStatement(sql);
-    ps.setString(1, allowanceName); 
-    ps.setDouble(2, amount);         
-    ps.setInt(3, empId);  
-    ps.executeUpdate();
-    		} catch (SQLException e) {
-    			// TODO Auto-generated catch block
-    			e.printStackTrace();
-    		}
+    // FOR ADDING ALLOWANCES TO THE TABLE
+    public void addAllowance(int empId, String allowanceName, double amount) {
 
-    
-}
-// TO GET TOTAL ALLOWANCE FOR AN EMPLOYEE
-public static double gettotalAllowance(int empId)
-         {
-    String sql =
-        "SELECT SUM(allowance_amount) FROM  employee_allowances WHERE empID= ?" ;
+        String sql = "INSERT INTO employee_allowances "
+                   + "(emp_id, allowance_name, amount) "
+                   + "VALUES (?, ?, ?)";
 
-    PreparedStatement ps =
-        conn.prepareStatement(sql);
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
 
-    ps.setInt(1, empId);
-    ResultSet rs =ps.executeQuery();
-    if(rs.next()) {
-    	return rs.getDouble("SUM(allowance_amount)");
+            ps.setInt(1, empId);
+            ps.setString(2, allowanceName);
+            ps.setDouble(3, amount);
+
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-   
-         }
 
-// total fulltimers & parttimers deduction separately
+    // FOR UPDATING TABLE
+    public void updateAllowance(int empId, String allowanceName, double amount) {
 
-public static  double gettaxPercentage(String tax_name,String emp_type)
-{
-String sql =
-"SELECT tax_per FROM  payroll WHERE tax_name= ? and emp_type= ?" ;
+        String sql = "UPDATE employee_allowances "
+                   + "SET allowance_name=?, amount=? "
+                   + "WHERE emp_id=?";
 
-PreparedStatement ps =
-conn.prepareStatement(sql);
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
 
-ps.setString(1, tax_name);
-ps.setString(2, emp_type );
+            ps.setString(1, allowanceName);
+            ps.setDouble(2, amount);
+            ps.setInt(3, empId);
 
-ResultSet rs =ps.executeQuery();
-if(rs.next()) {
-	return rs.getDouble("tax_per");
-}
+            ps.executeUpdate();
 
-}
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
+    // TO GET TOTAL ALLOWANCE FOR AN EMPLOYEE
+    public double getTotalAllowance(int empId) {
+
+        String sql =
+            "SELECT SUM(amount) AS total " +
+            "FROM employee_allowances " +
+            "WHERE emp_id = ?";
+
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+
+            ps.setInt(1, empId);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                return rs.getDouble("total");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return 0.0;
+    }
+
+    // GET TAX PERCENTAGE
+    public double getTaxPercentage(String taxName, String empType) {
+
+        String sql =
+            "SELECT tax_per " +
+            "FROM payroll " +
+            "WHERE tax_name = ? AND emp_type = ?";
+
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+
+            ps.setString(1, taxName);
+            ps.setString(2, empType);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                return rs.getDouble("tax_per");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return 0.0;
+    }
 }
--- src/service/PayrollService.java
+++ src/service/PayrollService.java
@@ -7,6 +7,7 @@ import model.Employee;
 public class PayrollService{
 	// SALARY CALCULATION
 	Employee emp=new Employee();
+	AllowanceDAO dao = new AllowanceDAO();
 	public double calculateNetSalary(Employee emp) {
 	
 		// salary from Employee in basicSalary
@@ -15,7 +16,7 @@ public class PayrollService{
 		
 		// to get total allowance
 		
-		double allowanceTotal= AllowanceDAO.gettotalAllowance(emp.getEmpId());
+		double allowanceTotal= dao.getTotalAllowance(emp.getEmpId());
 		
 		// adding allowance to basic salary
 		
@@ -25,28 +26,29 @@ public class PayrollService{
 		
 	    double deductionTotal=0;
 	    double altaX=0;
+	    
 	    if (emp.getEmpType().toUpperCase().equals("FULLTIME")) {
-	    	double IT=basicSalary*(AllowanceDAO.gettaxPercentage("INCOME TAX","FULLTIME"))/100;
-	    	double PT=basicSalary*(AllowanceDAO.gettaxPercentage("PROFESSIONAL TAX","FULLTIME"))/100;
-	    	double PF=basicSalary*(AllowanceDAO.gettaxPercentage("PROVIDENT FUND","FULLTIME"))/100;
-	    	double ESI=basicSalary*(AllowanceDAO.gettaxPercentage("ESI","FULLTIME"))/100;
-	    	double HI=basicSalary*(AllowanceDAO.gettaxPercentage("HEALTH INSURANCE","FULLTIME"))/100;
-	    	double PC=basicSalary*(AllowanceDAO.gettaxPercentage("PENSION CONTRIBUTION","FULLTIME"))/100;
+	    	double IT=basicSalary*(dao.getTaxPercentage("INCOME TAX","FULLTIME"))/100;
+	    	double PT=basicSalary*(dao.getTaxPercentage("PROFESSIONAL TAX","FULLTIME"))/100;
+	    	double PF=basicSalary*(dao.getTaxPercentage("PROVIDENT FUND","FULLTIME"))/100;
+	    	double ESI=basicSalary*(dao.getTaxPercentage("ESI","FULLTIME"))/100;
+	    	double HI=basicSalary*(dao.getTaxPercentage("HEALTH INSURANCE","FULLTIME"))/100;
+	    	double PC=basicSalary*(dao.getTaxPercentage("PENSION CONTRIBUTION","FULLTIME"))/100;
 	    	//ADDING
 	    	deductionTotal=IT+PT+PF+ESI+HI+PC;
 	    	// ALLOWANCE
-	    	double HRA=basicSalary*(AllowanceDAO.gettaxPercentage("HRA","FULLTIME"))/100;
-	    	double IA=basicSalary*(AllowanceDAO.gettaxPercentage("INTERNET ALLOWANCE","FULLTIME"))/100;
+	    	double HRA=basicSalary*(dao.getTaxPercentage("HRA","FULLTIME"))/100;
+	    	double IA=basicSalary*(dao.getTaxPercentage("INTERNET ALLOWANCE","FULLTIME"))/100;
              altaX=HRA+IA;
 	    }
 	    else
 	    {
 	    	//deduction
-	    	double IT=basicSalary*(AllowanceDAO.gettaxPercentage("INCOME TAX","PARTTIME"))/100;
-	    	double PT=basicSalary*(AllowanceDAO.gettaxPercentage("PROFESSIONAL TAX","PARTTIME"))/100;
+	    	double IT=basicSalary*(dao.getTaxPercentage("INCOME TAX","PARTTIME"))/100;
+	    	double PT=basicSalary*(dao.getTaxPercentage("PROFESSIONAL TAX","PARTTIME"))/100;
 	    	deductionTotal=IT+PT;
 	    	//allowance
-	    	double IA=basicSalary*(AllowanceDAO.gettaxPercentage("INTERNET ALLOWANCE","PARTTIME"))/100;
+	    	double IA=basicSalary*(dao.getTaxPercentage("INTERNET ALLOWANCE","PARTTIME"))/100;
 	    	altaX=IA;
 	    }
 	    double netSalary=grossSalary-deductionTotal+altaX;
