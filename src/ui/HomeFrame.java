b84dd689fbed42fd9563e723c716322ac0dfc3b4
AsherX7 <angelakaby6@gmail.com>
2026-06-04 11:40:34 +0530

    RESTORING

--- src/ui/HomeFrame.java
+++ src/ui/HomeFrame.java
@@ -1,5 +1,32 @@
 package ui;
 
-public class HomeFrame extends LoginFrame {
-	
+import javax.swing.*;
+import java.awt.*;
+
+
+
+public class HomeFrame extends JFrame {
+	private static final long serialVersionUID = 1L;
+	    public HomeFrame() {
+
+        setTitle("Employee Payroll System");
+        setSize(500, 400);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(3,1,20,20));
+
+        JButton modifyBtn = new JButton("Modify Employee");
+        JButton payslipBtn = new JButton("Payslip Calculation");
+        JButton payrollBtn = new JButton("Payroll Generator");
+
+        panel.add(modifyBtn);
+        panel.add(payslipBtn);
+        panel.add(payrollBtn);
+
+        add(panel);
+
+        setVisible(true);
+    }
 }
--- src/ui/LoginFrame.java
+++ src/ui/LoginFrame.java
@@ -1,13 +1,15 @@
 package ui;
 
+//import
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+//class
 
-public class LoginFrame  extends JFrame{
+public class LoginFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
-    private JTextField usernameField;
-    private JPasswordField passwordField;
+
+    
     private JButton loginButton;
 
     public LoginFrame() {
@@ -24,85 +26,43 @@ public class LoginFrame  extends JFrame{
         titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
         titleLabel.setBounds(120, 40, 400, 40);
 
-        JLabel logoLabel = new JLabel("src/ui/logo.png");
+        ImageIcon icon = new ImageIcon(
+                getClass().getResource("logo.png")
+        );
+
+        Image img = icon.getImage();
+        Image resizedImg = img.getScaledInstance(200,200, Image.SCALE_SMOOTH);
+
+        ImageIcon resizedIcon = new ImageIcon(resizedImg);
+
+        JLabel logoLabel = new JLabel(resizedIcon);
+        logoLabel.setBounds(250, 50, 100, 100);
+
+        panel.add(logoLabel);
         logoLabel.setHorizontalAlignment(SwingConstants.CENTER);
         logoLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         logoLabel.setBounds(225, 90, 150, 80);
 
-        JLabel userLabel = new JLabel("Username:");
-        userLabel.setBounds(140, 210, 100, 25);
+       
 
-        usernameField = new JTextField();
-        usernameField.setBounds(240, 210, 180, 25);
-
-        JLabel passLabel = new JLabel("Password:");
-        passLabel.setBounds(140, 250, 100, 25);
-
-        passwordField = new JPasswordField();
-        passwordField.setBounds(240, 250, 180, 25);
-
+        
         loginButton = new JButton("Login");
         loginButton.setBounds(250, 310, 100, 35);
-
-        loginButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                String username = usernameField.getText();
-                String password = new String(passwordField.getPassword());
-
-                if(username.equals("admin") && password.equals("admin123")) {
-
-                    new HomeFrame();
-                    dispose();
-
-                } else {
-
-                    JOptionPane.showMessageDialog(
-                            null,
-                            "Invalid Username or Password!"
-                    );
-                }
-            }
-
-			private void dispose() {
-				// TODO Auto-generated method stub
-				
-			}
+        
+        loginButton.addActionListener(e -> {
+        	 new HomeFrame();   // Open next page
+             dispose();         // Close login page
         });
-
         panel.add(titleLabel);
         panel.add(logoLabel);
-        panel.add(userLabel);
-        panel.add(usernameField);
-        panel.add(passLabel);
-        panel.add(passwordField);
+       
         panel.add(loginButton);
 
         add(panel);
         setVisible(true);
     }
 
-    private void setDefaultCloseOperation(int exitOnClose) {
-    	
-    }
-
-	private void setLocationRelativeTo(Object object) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private void setSize(int i, int j) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private void setTitle(String string) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         new LoginFrame();
     }
 }
