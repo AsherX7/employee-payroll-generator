29389dcca6469b02f342c97b1e6ac9ac7641bc44
AsherX7 <angelakaby6@gmail.com>
2026-06-07 12:15:43 +0530

    FINISHED

--- src/ui/HomeFrame.java
+++ src/ui/HomeFrame.java
@@ -3,12 +3,10 @@ package ui;
 import javax.swing.*;
 import java.awt.*;
 
-
-
 public class HomeFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
 	    public HomeFrame() {
-
+//heading
         setTitle("Employee Payroll System");
         setSize(500, 400);
         setLocationRelativeTo(null);
@@ -16,7 +14,7 @@ public class HomeFrame extends JFrame {
 
         JPanel panel = new JPanel();
         panel.setLayout(new GridLayout(3,1,20,20));
-
+//buttons
         JButton modifyBtn = new JButton("Modify Employee");
         JButton payslipBtn = new JButton("Payslip Calculation");
         JButton payrollBtn = new JButton("Payroll Generator");
